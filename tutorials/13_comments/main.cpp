ee8551aa4eeba4f455f3e5f8b182e90ab708b805
Zen Sepiol <zensepiol@pm.me>
2021-12-20 19:17:06 +0100

    Add code for video 13

---
+++ tutorials/13_comments/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+int main()
+{
+    // This is a comment 
+    std::cout << "Hello Friend" << std::endl;
+
+    float a = 0.F;
+    float b = 1.F;
+
+    // add a and b
+    float c = a + b;
+
+    return 0;
+}  
