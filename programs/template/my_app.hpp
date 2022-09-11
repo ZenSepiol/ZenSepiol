c40a102fe59f2a5803dd05c41c470de93dd2b403
Zen Sepiol <zensepiol@pm.me>
2022-09-11 14:21:06 +0200

    Update template App

--- programs/template/my_app.hpp
+++ programs/template/my_app.hpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include <stdio.h>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
 
 #include "app_base.hpp"
-
-template <typename T>
-void pop_front(std::vector<T>& vec)
-{
-    assert(!vec.empty());
-    vec.front() = std::move(vec.back());
-    vec.pop_back();
-}
 
 class MyApp : public AppBase<MyApp>
 {
@@ -22,21 +11,11 @@ class MyApp : public AppBase<MyApp>
 
     void StartUp()
     {
-        for (int i = 0; i < 50; ++i)
-        {
-            frame_times.push_back(0.f);
-        }
     }
 
     void Update()
     {
         ImGui::Begin("Tool");
-
-        // Edit a color (stored as ~4 floats)
-        ImGui::ColorEdit4("Color", my_color);
-
-        // Plot some values
-        ImGui::PlotLines("Frame Times", frame_times.data(), frame_times.size(), 0, 0, ImVec2(500, 500));
 
         ImGui::End();
     };
@@ -58,7 +37,4 @@ class MyApp : public AppBase<MyApp>
     }
 
   private:
-    float my_color[4];
-
-    std::vector<float> frame_times;
 };
