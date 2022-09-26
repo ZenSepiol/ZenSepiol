1be33a432716e8db2f590acf30eff87c845cd56a
Zen Sepiol <zensepiol@pm.me>
2022-09-26 22:02:01 +0200

    Code for next video

--- programs/tictactoe/tictactoe.hpp
+++ programs/tictactoe/tictactoe.hpp
@@ -3,6 +3,13 @@
 
 #include "app_base.hpp"
 
+enum class CellState
+{
+    Empty,
+    Circle,
+    Cross
+};
+
 class TicTacToe : public AppBase<TicTacToe>
 {
   public:
@@ -20,6 +27,58 @@ class TicTacToe : public AppBase<TicTacToe>
         ImGui::End();
 
         // ImDrawList* draw = ImGui::GetBackgroundDrawList();
+
+        for (int row = 0; row < 3; ++row)
+        {
+            for (int col = 0; col < 3; ++col)
+            {
+                DrawCell(row, col);
+            }
+        }
+    }
+
+    void DrawCell(int row, int col)
+    {
+        int cell_width = 100;
+        int cell_height = cell_width;
+
+        CellState cell_value = GetCell(row, col);
+
+        ImVec2 top_left = ImVec2(cell_width * col, cell_height * row);
+        ImVec2 bottom_right = ImVec2(cell_width * (col + 1), cell_height * (row + 1));
+
+        ImDrawList* draw = ImGui::GetBackgroundDrawList();
+        draw->AddRect(top_left, bottom_right, ImGui::GetColorU32(ImGuiCol_Text));
+
+        int symbol_size = 50;
+        ImVec2 center = ImVec2(cell_width/2 + top_left.x, cell_height/2 + top_left.y);
+        if (cell_value == CellState::Circle)
+        {
+          draw->AddCircle(center, symbol_size/2, ImGui::GetColorU32(ImGuiCol_Text));
+        }
+        else if (cell_value == CellState::Cross)
+        {
+          ImVec2 tl(center.x - symbol_size/2, center.y - symbol_size/2);
+          ImVec2 br(center.x + symbol_size/2, center.y + symbol_size/2);
+          draw->AddLine(tl, br, ImGui::GetColorU32(ImGuiCol_Text));
+
+          ImVec2 tr(center.x + symbol_size/2, center.y - symbol_size/2);
+          ImVec2 bl(center.x - symbol_size/2, center.y + symbol_size/2);
+          draw->AddLine(tr, bl, ImGui::GetColorU32(ImGuiCol_Text));
+        }
+        else
+        {
+        }
+
+        if (mouse_x > top_left.x && mouse_x < bottom_right.x && mouse_y > top_left.y && mouse_y < bottom_right.y && cell_value == CellState::Empty)
+        {
+          draw->AddRect(top_left, bottom_right, ImGui::GetColorU32(ImGuiCol_Text), 0.0f, 0, 4.0);
+        }
+    }
+
+    CellState GetCell(int row, int col)
+    {
+        return game_state[row][col];
     }
 
     static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
@@ -27,7 +86,16 @@ class TicTacToe : public AppBase<TicTacToe>
         // ONLY forward mouse data to your underlying app/game.
         ImGuiIO& io = ImGui::GetIO();
         if (!io.WantCaptureMouse)
-            std::cout << "Button Clicked!: " << button << std::endl;
+        {
+            if (button == ImGuiMouseButton_Left && action == 1)
+            {
+                clicked = true;
+            }
+            else if (button == ImGuiMouseButton_Left && action == 0)
+            {
+                clicked = false;
+            }
+        }
     }
 
     static void CursorPosCallback(GLFWwindow* window, double xpos, double ypos)
@@ -35,8 +103,25 @@ class TicTacToe : public AppBase<TicTacToe>
         // ONLY forward mouse data to your underlying app/game.
         ImGuiIO& io = ImGui::GetIO();
         if (!io.WantCaptureMouse)
-            std::cout << "Cursor moved! x: " << xpos << " y: " << ypos << std::endl;
+        {
+            mouse_x = xpos;
+            mouse_y = ypos;
+        }
     }
 
   private:
+    std::array<std::array<CellState, 3>, 3> game_state = {
+        {{{CellState::Empty, CellState::Empty, CellState::Empty}},
+         {{CellState::Cross, CellState::Cross, CellState::Cross}},
+         {{CellState::Circle, CellState::Circle, CellState::Circle}}}};
+
+    static int mouse_x;
+    static int mouse_y;
+
+    static bool clicked;
 };
+
+int TicTacToe::mouse_x = 0;
+int TicTacToe::mouse_y = 0;
+
+bool TicTacToe::clicked = false;
